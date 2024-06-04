7d75ed1f4d08319fe3874b956d6c8e41076c0ddc
Jayesh Gajarkar <jayeshgajarkar@gmail.com>
2024-06-04 18:30:55 +0530

    Update Assignment_01.py

--- Assignment_01.py
+++ Assignment_01.py
@@ -13,9 +13,21 @@ def print_upper_triangle(n):
             print("*",end=" ")
         print()
 
+# Pyramid
+def print_pyramid(n):
+    for i in range(n):
+        for j in range(n - i - 1):
+            print(" ", end=" ")
+        for k in range(2 * i + 1):
+            print("*", end=" ")
+        print()
+
 n = 5
 print("Lower Triangle Pattern:")
 print_lower_triangle(n)
 
 print("Upper Triangle Pattern:")
 print_upper_triangle(n)
+
+print("Pyramid")
+print_pyramid(n)
